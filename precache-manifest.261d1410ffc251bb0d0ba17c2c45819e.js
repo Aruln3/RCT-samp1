fd01942eea61b2ab853e3ca0d544d3098d44a95d
ARUL D <aruln311@gmail.com>
2022-10-20 00:05:49 +0530

    Add files via upload

---
+++ precache-manifest.261d1410ffc251bb0d0ba17c2c45819e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ecc2cc0ce4183e0def",
+    "url": "/static/css/main.842970bb.chunk.css"
+  },
+  {
+    "revision": "f3ecc2cc0ce4183e0def",
+    "url": "/static/js/main.f3ecc2cc.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "87c090448ce002f10410",
+    "url": "/static/css/2.5faa8acf.chunk.css"
+  },
+  {
+    "revision": "87c090448ce002f10410",
+    "url": "/static/js/2.87c09044.chunk.js"
+  },
+  {
+    "revision": "c1b6e4f0035755e25821",
+    "url": "/static/js/runtime~main.c1b6e4f0.js"
+  },
+  {
+    "revision": "b38de9970adaa48a94aab227d1e69874",
+    "url": "/static/media/backg.b38de997.svg"
+  },
+  {
+    "revision": "9e4d452ea9bdac4add6d41c807ba1057",
+    "url": "/index.html"
+  }
+];
